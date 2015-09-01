bdc9368c889a710f3d63db73f01ea9f124b57c6c
KjetilFjellheim <kjetil.fjellheim@imr.no>
2015-09-01 16:25:20 +0200

    Updated service layer and dao version

--- service/src/main/java/no/imr/nmdapi/surveytimeseries/service/NMDSurveyTimeSeriesServiceImpl.java
+++ service/src/main/java/no/imr/nmdapi/surveytimeseries/service/NMDSurveyTimeSeriesServiceImpl.java
@@ -2,9 +2,11 @@ package no.imr.nmdapi.surveytimeseries.service;
 
 import java.util.List;
 import no.imr.nmd.commons.surveytimeseries.jaxb.SurveyTimeSeriesType;
-import no.imr.nmdapi.dao.file.NMDDataDao;
+import no.imr.nmdapi.dao.file.NMDSeriesReferenceDao;
+import no.imr.nmdapi.exceptions.BadRequestException;
 import no.imr.nmdapi.generic.response.v1.ListElementType;
 import no.imr.nmdapi.generic.response.v1.ResultElementType;
+import org.apache.commons.configuration.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -14,38 +16,46 @@ import org.springframework.beans.factory.annotation.Autowired;
  */
 public class NMDSurveyTimeSeriesServiceImpl implements NMDSurveyTimeSeriesService {
 
+    /**
+     * Data type.
+     */
+    private static final String TYPE = "surveytimeseries";
+
     @Autowired
-    private NMDDataDao nmdDataDao;
+    private NMDSeriesReferenceDao seriesReferenceDao;
+
+    @Autowired
+    private Configuration configuration;
 
     @Override
     public Object getData(final String name) {
-        return nmdDataDao.get(name, SurveyTimeSeriesType.class);
+        return seriesReferenceDao.get(name, SurveyTimeSeriesType.class.getPackage().getName());
     }
 
     @Override
     public void deleteData(final String name) {
-        nmdDataDao.delete(name);
+        seriesReferenceDao.delete(TYPE, name, true);
     }
 
    @Override
-    public void insertData(final String name, final SurveyTimeSeriesType seriesType) {
-        nmdDataDao.insert(name, seriesType, SurveyTimeSeriesType.class);
+    public void insertData(final String name, final SurveyTimeSeriesType surveyTimeSeriesType) {
+        if (!name.equalsIgnoreCase(surveyTimeSeriesType.getSurveytimeseriesname())) {
+            throw new BadRequestException("Cruiseserie name is not equal to value in the data.");
+        }
+        String readRole = configuration.getString("default.readrole");
+        String writeRole = configuration.getString("default.writerole");
+        String owner = configuration.getString("default.owner");
+        seriesReferenceDao.insert(writeRole, readRole, owner, TYPE, name, surveyTimeSeriesType, true);
     }
 
-
     @Override
-    public void updateData(final String name, final SurveyTimeSeriesType seriesType) {
-        nmdDataDao.update(name, seriesType, SurveyTimeSeriesType.class);
-    }
-
-    @Override
-    public boolean hasData(String name) {
-        return nmdDataDao.hasData(name);
+    public void updateData(final String name, final SurveyTimeSeriesType surveyTimeSeriesType) {
+        seriesReferenceDao.update(name, surveyTimeSeriesType);
     }
 
     @Override
     public ListElementType list() {
-        List<String> names = nmdDataDao.listSeries();
+        List<String> names = seriesReferenceDao.list();
         ListElementType elementType = new ListElementType();
         for (String name : names) {
             ResultElementType resultElementType = new ResultElementType();
@@ -53,6 +63,11 @@ public class NMDSurveyTimeSeriesServiceImpl implements NMDSurveyTimeSeriesServic
             elementType.getElement().add(resultElementType);
         }
         return elementType;
+    }
+
+    @Override
+    public boolean hasData(String name) {
+        return seriesReferenceDao.hasData(name);
     }
 
 }
